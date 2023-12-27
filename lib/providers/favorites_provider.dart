5946a73f3dec22838a45f90e37726f6ecafa782f
MoldyHacker <56985918+MoldyHacker@users.noreply.github.com>
2023-12-27 15:15:01 -0600

    created basic favorites provider

---
+++ lib/providers/favorites_provider.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:meals/models/meal.dart';
+
+class FavoriteMealsNotifier extends StateNotifier<List<Meal>> {
+  FavoriteMealsNotifier() : super([]);
+
+  void toggleMealFavoriteStatus(Meal meal) {
+    final mealIsFavorite = state.contains(meal);
+
+    if (mealIsFavorite) {
+      state = state.where((m) => m.id != meal.id).toList();
+      // state = [...state]..remove(meal);
+    } else {
+      state = [...state, meal];
+      // state = [...state]..add(meal);
+    }
+  }
+}
+
+final favoriteMealsProvider =
+    StateNotifierProvider<FavoriteMealsNotifier, List<Meal>>(
+        (ref) => FavoriteMealsNotifier());
